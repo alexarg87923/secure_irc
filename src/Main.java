ad446d644a0ef58415e451af5cd1310f3806aa39
Alex Arguelles <alexarguelles87923@gmail.com>
2024-11-22 13:30:33 -0500

    Beginning GUI & Abstracting IO class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+	private static IO io = new IO();
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        io.print_buff("Welcome to Wormhole IRC");
+		io.print_buff("1. Login");
+		io.print_flush("2. SignUp");
+		
+		try {
+			switch(io.get_inp_with_cursor()) {
+				case "1":
+				io.print_flush("Login");
+				break;
+				case "2":
+				io.print_flush("Signup");
+				break;
+				default:
+				io.print_flush("Invalid Input.");
+			}
+		} catch (Exception e) {
+			io.print_flush("Invalid Input.");
+		}
+
+		io.close();
+		return;
     }
 }
