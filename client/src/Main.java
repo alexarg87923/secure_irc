de58c38bd59366cc9543fe32c099be4f79e8b502
Alex Arguelles <alexarguelles87923@gmail.com>
2024-12-03 01:33:23 -0500

    Start PKI Infra

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,24 +1,104 @@
+import java.io.*;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
+import java.util.Map;
+import javax.net.ssl.*;
+import java.security.cert.Certificate;
+
 public class Main {
-	private static IO io = new IO();
-    public static void main(String[] args) {
-        io.print_buff("Welcome to Wormhole IRC");
-		io.print_buff("1. Login");
-		io.print_flush("2. SignUp");
-		
+	public static void main(String[] args) {
+		IO io = new IO();
+		Map<String, String> env = EnvLoader.loadEnv();
+		String keyPath = "client.keystore";
+		String trustPath = "client.truststore";
+		File keyFile = new File(keyPath);
+		File trustFile = new File(trustPath);
+
 		try {
-			switch(io.get_inp_with_cursor()) {
-				case "1":
-				Authentication.login();
-				break;
-				case "2":
-				Authentication.signup();
-				break;
-				default:
-				io.print_flush("Invalid Input.");
+			KeyStore keyStore = KeyStore.getInstance("JKS");
+			KeyStore trustStore = KeyStore.getInstance("JKS");
+
+			if (!trustFile.exists()) {
+				io.print_flush("Truststore doesn't exist");
+				trustStore.load(null, null);
+
+				try (FileOutputStream fos = new FileOutputStream(trustFile)) {
+					trustStore.store(fos, env.get("TRUST_STORE_PASSWORD").toCharArray());
+				}
+				io.print_flush("Truststore created");
+			} else {
+				io.print_flush("Truststore exists");
+				try (FileInputStream trustStoreFile = new FileInputStream(trustFile)) {
+					trustStore.load(trustStoreFile, null);
+				}
+				io.print_flush("Truststore loaded");
 			}
-		} catch (Exception e) {
-			io.print_flush("Invalid Input.");
-		}
+
+			if (!keyFile.exists()) {
+				io.print_flush("Keystore doesn't exist");
+				keyStore.load(null, null);
+
+				try (FileOutputStream fos = new FileOutputStream(keyPath)) {
+					keyStore.store(fos, env.get("KEY_STORE_PASSWORD").toCharArray());
+				}
+				io.print_flush("Keystore created");
+			} else {
+				io.print_flush("Keystore exists");
+				try (FileInputStream keyStoreFile = new FileInputStream(keyPath)) {
+					keyStore.load(keyStoreFile, env.get("KEY_STORE_PASSWORD").toCharArray());
+				}
+				io.print_flush("Keystore loaded");
+			}
+
+			String alias = "ca_server_public";
+			if (trustStore.containsAlias(alias)) {
+				io.print_flush("Server public certificate exists");
+			} else {
+				io.print_flush("Server public certificate doesn't exist");
+				String server_ca = alias+".crt";
+
+				CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+				try (FileInputStream fis = new FileInputStream(server_ca)) {
+					Certificate cert = certFactory.generateCertificate(fis);
+					trustStore.setCertificateEntry(alias, cert);
+					io.print_flush("Certificate added with alias: " + alias);
+
+					try (FileOutputStream fos = new FileOutputStream(trustFile)) {
+						trustStore.store(fos, env.get("TRUST_STORE_PASSWORD").toCharArray());
+						io.print_flush("TrustStore updated successfully with server's public cert.");
+					}
+				} catch (Exception e) {
+					io.print_flush("Failed to add certificate: " + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+
+			TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X.509");
+			trustManagerFactory.init(trustStore);
+
+			KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+			keyManagerFactory.init(keyStore, env.get("KEY_STORE_PASSWORD").toCharArray());
+
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new SecureRandom());
+
+			SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+			SSLSocket socket = (SSLSocket) socketFactory.createSocket(env.get("IP"), Integer.parseInt(env.get("PORT")));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			writer.write("Test");
+			writer.flush();
+
+
+            String response = reader.readLine();
+            io.print_flush("Server" + response);
+
+            socket.close();
+        } catch (Exception e) {
+            io.print_flush("Error: " + e.getMessage());
+        }
 
 		io.close();
 		return;
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,41 +1,118 @@
 import javax.net.ssl.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.KeyFactorySpi;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.*;
+import java.security.PrivateKey;
 
 public class Main {
-	private static String keyPath = "server.keystore";
 	private static IO io = new IO();
 
     public static void main(String[] args) throws Exception {
 		Map<String, String> env = EnvLoader.loadEnv();
-		File file = new File(keyPath);
-		KeyStore keyStore = KeyStore.getInstance("JKS");
+		String keyPath = "server.keystore";
+		String trustPath = "server.truststore";
+		File keyFile = new File(keyPath);
+		File trustFile = new File(trustPath);
 
-		if (!file.exists()) {
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		KeyStore trustStore = KeyStore.getInstance("JKS");
+
+		if (!trustFile.exists()) {
+			io.print_flush("Truststore doesn't exist");
+			trustStore.load(null, null);
+
+			try (FileOutputStream fos = new FileOutputStream(trustFile)) {
+				trustStore.store(fos, env.get("TRUST_STORE_PASSWORD").toCharArray());
+			}
+			io.print_flush("Truststore created");
+        } else {
+			io.print_flush("Truststore exists");
+			try (FileInputStream trustStoreFile = new FileInputStream(trustFile)) {
+				trustStore.load(trustStoreFile, env.get("TRUST_STORE_PASSWORD").toCharArray());
+			}
+			io.print_flush("Truststore loaded");
+		}
+
+		if (!keyFile.exists()) {
 			io.print_flush("Keystore doesn't exist");
 			keyStore.load(null, null);
 
-			try (FileOutputStream fos = new FileOutputStream(keyPath)) {
-				keyStore.store(fos, env.get("PASSWORD").toCharArray());
+			try (FileOutputStream fos = new FileOutputStream(keyFile)) {
+				keyStore.store(fos, env.get("KEY_STORE_PASSWORD").toCharArray());
+			}
+			io.print_flush("Keystore created");
+			String serverKeyPath = "server.key";
+			File serverKeyFile = new File(serverKeyPath);
+			File rootCaKeyFile = new File("rootCA.crt");
+			File serverCAKeyFile = new File("server.crt");
+
+			if (!serverKeyFile.exists() || !rootCaKeyFile.exists()) {
+				io.print_flush("Server Key or rootCA doesn't exist");
+			} else {
+				io.print_flush("Server Key and root CA does exist");
+
+				CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+				Certificate rootCA = null;
+				Certificate serverCA = null;
+
+				try (FileInputStream fis = new FileInputStream(rootCaKeyFile)) {
+					rootCA = certFactory.generateCertificate(fis);
+				} catch (Exception e) {
+					io.print_flush("Failed to get certificate: " + e.getMessage());
+				}
+
+				try (FileInputStream fis = new FileInputStream(serverCAKeyFile)) {
+					serverCA = certFactory.generateCertificate(fis);
+				} catch (Exception e) {
+					io.print_flush("Failed to get certificate: " + e.getMessage());
+				}
+				
+				String key = new String(Files.readAllBytes(Paths.get(serverKeyPath)));
+				key = key.replace("-----BEGIN PRIVATE KEY-----", "")
+						.replace("-----END PRIVATE KEY-----", "")
+						.replaceAll("\\s", "");
+				
+
+				byte[] keyBytes = Base64.getDecoder().decode(key);
+				PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+				KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+				PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+				keyStore.setKeyEntry("server_private_key", privateKey, env.get("KEY_STORE_PASSWORD").toCharArray(), new Certificate[]{rootCA, serverCA});
+				
+				try (FileOutputStream keyStoreFile = new FileOutputStream(keyFile)) {
+					keyStore.store(keyStoreFile, env.get("KEY_STORE_PASSWORD").toCharArray());
+				}
 			}
         } else {
 			io.print_flush("Keystore exists");
-			try (FileInputStream keyStoreFile = new FileInputStream(keyPath)) {
-				keyStore.load(keyStoreFile, env.get("PASSWORD").toCharArray());
+			try (FileInputStream keyStoreFile = new FileInputStream(keyFile)) {
+				keyStore.load(keyStoreFile, env.get("KEY_STORE_PASSWORD").toCharArray());
 			}
+			io.print_flush("Keystore loaded");
 		}
 
+		TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X.509");
+		trustManagerFactory.init(trustStore);
+
         KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-        keyManagerFactory.init(keyStore, env.get("PASSWORD").toCharArray());
-
-
+        keyManagerFactory.init(keyStore, env.get("KEY_STORE_PASSWORD").toCharArray());
 
         SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), new SecureRandom());
 
         SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
-        SSLServerSocket serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(8443);
+        SSLServerSocket serverSocket = (SSLServerSocket) serverSocketFactory.createServerSocket(Integer.parseInt(env.get("PORT")));
         io.print_flush("Secure server is listening on port " + env.get("PORT"));
 		env.clear();
 		env = null;
@@ -51,7 +128,7 @@ public class Main {
              BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
             String input;
             while ((input = in.readLine()) != null) {
-                System.out.println("Received: " + input);
+                io.print_flush("Received: " + input);
                 out.write("Echo: " + input + "\n");
                 out.flush();
             }
